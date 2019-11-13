527830cea5f523ebad78885b83d808c9ceb72050
liuyanyansun <1694614518@qq.com>
2019-11-13 14:28:02 +0800

    message

--- Blog/routes/index.js
+++ Blog/routes/index.js
@@ -11,13 +11,13 @@ router.post('/login', function(req, res, next) {
   var req1 = req.body;
 
   for (var i = 0; i < users.length; i++) {
-      if (req1.username == users[i].username && req1.pwd == users[i].password) {
-          res.render('list', { chapterList: chapterList });
-          return ;
+      if (req1.pwd == users[i].password && req1.username == users[i].username) {
+          res.render('list', {chapterList:chapterList});
+          return;
       }
   }
   res.set('Content-Type','text/plain; charset=utf-8');    
-  res.end("用户名密码错误");
+  res.end("用户名或密码错误");
 });
 
 module.exports = router;
